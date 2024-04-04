8ae14dac3d48990541921c76512a3d3792c3ab4f
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-04 13:39:11 +0300

    Get the city objects from request

--- JS Applications/3-Asynchronous programming and Promises/Exercise/01. FORECASTER/app.js
+++ JS Applications/3-Asynchronous programming and Promises/Exercise/01. FORECASTER/app.js
@@ -1,5 +1,19 @@
 function attachEvents() {
-    console.log("TODO...");
+    let submitButton = document.getElementById('submit');
+    let locationInput = document.getElementById('location');
+    let baseUrl = 'http://localhost:3000/';
+
+    submitButton.addEventListener('click', function () {
+        let cityValue = locationInput.value;
+
+        fetch(baseUrl + 'locations')
+            .then(res => res.json())
+            .then(res => {
+                let currCity = res.find(x => x.name === cityValue);
+                let code = currCity.code;
+            })
+
+    });
 }
 
 attachEvents();
