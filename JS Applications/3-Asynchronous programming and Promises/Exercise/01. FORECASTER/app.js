68e6fe88af9474b4f4003697dc00019cb3bc5ee6
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-04 14:11:33 +0300

    Retrive succesfully forecast for location and set the db one more time

--- JS Applications/3-Asynchronous programming and Promises/Exercise/01. FORECASTER/app.js
+++ JS Applications/3-Asynchronous programming and Promises/Exercise/01. FORECASTER/app.js
@@ -10,18 +10,21 @@ function attachEvents() {
             .then(res => res.json())
             .then(res => {
                 let currCity = res.find(x => x.name === cityValue);
-                let code = currCity.code;
+                let { code } = currCity;
 
                 Promise.all([
-                    fetch(baseUrl + `forecast/today/${code}`),
-                    fetch(baseUrl + `forecast/upcoming/${code}`)
+                    fetch(baseUrl + `today?code=${code}`),
+                    fetch(baseUrl + `upcoming?code=${code}`)
                 ])
-                .then(res => Promise.all(res).map(x => x.json()))
-                .then(([today, upcomming]) => {
-                    console.log(today);
-                    console.log(upcomming);
-                })
+                    .then(res => Promise.all((res).map(x => x.json())))
+                    .then(([todayData, upcomingData]) => {
+                        console.log("Today's weather: ", todayData);
+                        console.log("Upcoming weather: ", upcomingData);
+                    })
+                    .catch(err => console.error(err));
             })
+            .catch(err => console.error(err));
+
 
     });
 }
