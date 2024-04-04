d7ec00cf396bfbf24c2f4e9823a190d2dec90c40
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-04 19:30:06 +0300

    Create GET all catches functionality

--- JS Applications/3-Asynchronous programming and Promises/Exercise/02. FISHER-GAME/app.js
+++ JS Applications/3-Asynchronous programming and Promises/Exercise/02. FISHER-GAME/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
     let baseUrl = 'http://localhost:3000/catches';
+
+    let loadButton = document.querySelector('.load');
+
+    const mainElement = document.querySelector('#catches');
+
+    loadButton.addEventListener('click', function () {
+        fetch(baseUrl)
+            .then(res => res.json())
+            .then(catches => {
+                catches.forEach(catchObj => {
+                    renderCatches(catchObj, mainElement);
+                });
+            })
+    });
+}
+
+function renderCatches(catchObject, mainElement) {
+    let currCatchDiv = document.createElement('div');
+    currCatchDiv.classList.add('catch');
+    currCatchDiv.setAttribute('data-id', catchObject.id);
+
+    currCatchDiv.innerHTML = `
+        <label>Angler</label>
+        <input type="text" class="angler" value="${catchObject.angler}" />
+        <hr>
+        <label>Weight</label>      
+        <input type="number" class="weight" value="${catchObject.weight}" />
+        <hr>
+        <label>Species</label>
+        <input type="text" class="species" value="${catchObject.species}" />
+        <hr>
+        <label>Location</label>
+        <input type="text" class="location" value="${catchObject.location}" />
+        <hr>
+        <label>Bait</label>
+        <input type="text" class="bait" value="${catchObject.bait}" />
+        <hr>
+        <label>Capture Time</label>
+        <input type="number" class="captureTime" value="${catchObject.captureTime}" />
+        <hr>
+        <button class="update">Update</button>
+        <button class="delete">Delete</button>
+    `;
+
+    mainElement.appendChild(currCatchDiv);
 }
 
 attachEvents();
