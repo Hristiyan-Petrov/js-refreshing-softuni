12ff0a62871e9ed39ba97e94c585dd05d7a2fa47
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-04 20:44:37 +0300

    Create PUT request functionality - update one of the current catch objects (div)

--- JS Applications/3-Asynchronous programming and Promises/Exercise/02. FISHER-GAME/app.js
+++ JS Applications/3-Asynchronous programming and Promises/Exercise/02. FISHER-GAME/app.js
@@ -19,25 +19,27 @@ function attachEvents() {
     addButton.addEventListener('click', function (e) {
         e.preventDefault();
 
-        let form = document.querySelector('#addForm');
+        let formElement = document.querySelector('#addForm');
 
-        let anglerInput = form.querySelector('.angler');
-        let weightInput = form.querySelector('.weight');
-        let speciesInput = form.querySelector('.species');
-        let locationInput = form.querySelector('.location');
-        let baitInput = form.querySelector('.bait');
-        let captureTimeInput = form.querySelector('.captureTime');
+        let bodyData = createBodyData(formElement);
 
-        let bodyData = (JSON.stringify({
-            "angler": anglerInput.value,
-            "weight": weightInput.value,
-            "species": speciesInput.value,
-            "location": locationInput.value,
-            "bait": baitInput.value,
-            "captureTime": captureTimeInput.value
-        }));
+        // let anglerInput = form.querySelector('.angler');
+        // let weightInput = form.querySelector('.weight');
+        // let speciesInput = form.querySelector('.species');
+        // let locationInput = form.querySelector('.location');
+        // let baitInput = form.querySelector('.bait');
+        // let captureTimeInput = form.querySelector('.captureTime');
 
-        console.log(bodyData);
+        // let bodyData = (JSON.stringify({
+        //     "angler": anglerInput.value,
+        //     "weight": weightInput.value,
+        //     "species": speciesInput.value,
+        //     "location": locationInput.value,
+        //     "bait": baitInput.value,
+        //     "captureTime": captureTimeInput.value
+        // }));
+
+        // console.log(bodyData);
 
         fetch(baseUrl, {
             method: 'POST',
@@ -57,36 +59,41 @@ function attachEvents() {
     mainElement.addEventListener('click', function (e) {
         e.preventDefault();
 
-        if (e.target.classList.value === 'delete') {
-            let id = e.target.parentElement.attributes.getNamedItem('data-id').value
-            let deleteUrl = baseUrl + `/${id}`;
-            deleteCatchObject(deleteUrl, id);
-        }
-
-        if (e.target.classList.value !== 'update') {
+        if (e.target.classList.value !== 'delete' && e.target.classList.value !== 'update') {
             return;
         }
 
+        let id = e.target.parentElement.attributes.getNamedItem('data-id').value
+        let currentUrl = baseUrl + `/${id}`;
 
+        // Delete button
+        if (e.target.classList.value === 'delete') {
+            deleteCatchObject(currentUrl, id);
+        }
+
+        // Update button
+        if (e.target.classList.value === 'update') {
+            updateCatchObject(mainElement, currentUrl, id);;
+        }
     });
 }
 
-function deleteCatchObject(deleteUrl, id) {
+function createBodyData(element) {
+    let anglerInput = element.querySelector('.angler');
+    let weightInput = element.querySelector('.weight');
+    let speciesInput = element.querySelector('.species');
+    let locationInput = element.querySelector('.location');
+    let baitInput = element.querySelector('.bait');
+    let captureTimeInput = element.querySelector('.captureTime');
 
-    fetch(deleteUrl, {
-        method: 'DELETE'
-    })
-        .then(res => {
-
-            if (res.ok) {
-                console.log(`Deleted item with id: ${id}`);
-            } else {
-                throw new Error(`Delete request failed with status ${res.status}`);
-            }
-        })
-        .catch(error => {
-            console.error('An error occurred:', error);
-        });
+    return (JSON.stringify({
+        "angler": anglerInput.value,
+        "weight": weightInput.value,
+        "species": speciesInput.value,
+        "location": locationInput.value,
+        "bait": baitInput.value,
+        "captureTime": captureTimeInput.value
+    }));
 }
 
 function renderCatches(catchObject, mainElement) {
@@ -95,7 +102,7 @@ function renderCatches(catchObject, mainElement) {
     currCatchDiv.setAttribute('data-id', catchObject.id);
 
     currCatchDiv.innerHTML = `
-        <label>Angler</label>
+    <label>Angler</label>
         <input type="text" class="angler" value="${catchObject.angler}" />
         <hr>
         <label>Weight</label>      
@@ -115,9 +122,49 @@ function renderCatches(catchObject, mainElement) {
         <hr>
         <button class="update">Update</button>
         <button class="delete">Delete</button>
-    `;
+        `;
 
     mainElement.appendChild(currCatchDiv);
+}
+
+function deleteCatchObject(currentUrl, id) {
+
+    fetch(currentUrl, {
+        method: 'DELETE'
+    })
+        .then(res => {
+
+            if (res.ok) {
+                console.log(`Deleted item with id: ${id}`);
+            } else {
+                throw new Error(`Delete request failed with status ${res.status}`);
+            }
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
+}
+
+function updateCatchObject(mainElement, currentUrl, id) {
+
+    let currentCatchDiv = Array.from(mainElement.children).find(catchDiv => catchDiv.attributes.getNamedItem('data-id').value === id);
+    let bodyData = createBodyData(currentCatchDiv);
+
+    fetch(currentUrl, {
+        method: 'PUT',
+        body: bodyData
+    })
+        .then(res => {
+
+            if (res.ok) {
+                console.log(`Updated item with id: ${id}`);
+            } else {
+                throw new Error(`Update request failed with status ${res.status}`);
+            }
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
 }
 
 attachEvents();
