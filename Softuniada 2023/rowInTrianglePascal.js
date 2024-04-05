30e39098bdea805f01e0f0017863c6d4db206882
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-05 10:08:12 +0300

    Solve first task from Softuniada

---
+++ Softuniada 2023/rowInTrianglePascal.js
@@ -0,0 +1,15 @@
+function getPascalRow(n) {
+    let row = [1];
+
+    for(let i = 0; i < n; i++) {
+        row.unshift(0);
+        
+        for(let j = 0; j < i + 1; j++) {
+            row[j] = row[j] + row[j + 1];
+        }
+    }
+    return row;
+}
+
+let res = getPascalRow(5);
+console.log(res);
