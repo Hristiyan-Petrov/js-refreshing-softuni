b8eb27d658c93ee9a90168870ce248383f0f5621
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-07 13:40:32 +0300

    Order the file and add comment lines

--- JS Applications/4-Remote databases and Firebase/Exercise/BOOKS/app.js
+++ JS Applications/4-Remote databases and Firebase/Exercise/BOOKS/app.js
@@ -5,6 +5,7 @@ const addButton = addBookForm.querySelector('#add-button');
 
 const baseUrl = 'https://js-app-ex-rem-dbs-blogs-2024-default-rtdb.firebaseio.com/books';
 
+// Get all books - GET request
 getAllBooksButton.addEventListener('click', function (e) {
     fetch(baseUrl + '.json')
         .then(res => res.json())
@@ -16,6 +17,84 @@ getAllBooksButton.addEventListener('click', function (e) {
         .catch(err => {
             console.log(err.message);
         });
+});
+
+// Add book - POST request
+addButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let isbn = document.getElementById('isbn').value;
+
+
+    let bodyData = {
+        title,
+        author,
+        isbn
+    }
+
+    fetch(`${baseUrl}.json`, {
+        method: 'POST',
+        body: JSON.stringify(bodyData)
+    })
+        .then(res => res.json())
+        .then(bookId => {
+            console.log(bookId);
+
+            let tableBodyEl = tableWrapper.querySelector('tbody');
+
+            // If table is opened (GET all books)
+            if (tableBodyEl) {
+                // Add dynamically new row to the table after POST request
+                let tr = document.createElement('tr');
+                fillUpTr(bodyData, tr);
+
+                // Append tr to tbody
+                let tbody = tableWrapper.querySelector('tbody');
+                tbody.appendChild(tr);
+            }
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+});
+
+// Delete and Update functionality - DELETE & PACTH request
+tableWrapper.addEventListener('click', function (e) {
+
+    if (e.target.tagName !== 'BUTTON') {
+        return;
+    }
+
+    // Delete functionality
+    if (e.target.textContent === 'Delete') {
+        let tr = e.target.closest('tr');
+        let bookId = tr.getAttribute('data-book-id');
+
+        // DELETE request to the server (Firebase in this case)
+        fetch(`${baseUrl}/${bookId}.json`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('HTTP error ' + res.status);
+                }
+                console.log('Succesfully deleted');
+                return res.json();
+            }).then(res => {
+                console.log(res);
+            }).catch(() => {
+                console.log("An error occurred while trying to delete the item.");
+            });
+
+    }
+
+
+    // Update functionality
+    // if () {
+
+    // }
 });
 
 function createTableDOM(bookData) {
@@ -73,46 +152,6 @@ function createTableDOM(bookData) {
     table.appendChild(tbody);
 }
 
-addButton.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let isbn = document.getElementById('isbn').value;
-
-
-    let bodyData = {
-        title,
-        author,
-        isbn
-    }
-
-    fetch(`${baseUrl}.json`, {
-        method: 'POST',
-        body: JSON.stringify(bodyData)
-    })
-        .then(res => res.json())
-        .then(bookId => {
-            console.log(bookId);
-
-            let tableBodyEl = tableWrapper.querySelector('tbody');
-
-            // If table is opened (GET all books)
-            if (tableBodyEl) {
-                // Add dynamically new row to the table after POST request
-                let tr = document.createElement('tr');
-                fillUpTr(bodyData, tr);
-                
-                // Append tr to tbody
-                let tbody = tableWrapper.querySelector('tbody');
-                tbody.appendChild(tr);
-            }
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
-});
-
 function fillUpTr(bodyData, tr) {
     // Create td elements for Title, Author, Isbn, and Buttons
     let tdTitle = document.createElement('td');
@@ -138,39 +177,3 @@ function fillUpTr(bodyData, tr) {
     tr.appendChild(tdIsbn);
     tr.appendChild(tdButtons);
 }
-
-tableWrapper.addEventListener('click', function (e) {
-
-    if (e.target.tagName !== 'BUTTON') {
-        return;
-    }
-
-    // Delete functionality
-    if (e.target.textContent === 'Delete') {
-        let tr = e.target.closest('tr');
-        let bookId = tr.getAttribute('data-book-id');
-
-        // DELETE request to the server (Firebase in this case)
-        fetch(`${baseUrl}/${bookId}.json`, {
-            method: 'DELETE'
-        })
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('HTTP error ' + res.status);
-                }
-                console.log('Succesfully deleted');
-                return res.json();
-            }).then(res => {
-                console.log(res);
-            }).catch(() => {
-                console.log("An error occurred while trying to delete the item.");
-            });
-
-    }
-
-
-    // Update functionality
-    // if () {
-
-    // }
-})
