121b3ebf4c60b345f469b9ce866dfda8ff84ac70
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-07 10:08:39 +0300

    Add firebase init file

---
+++ JS Applications/4-Remote databases and Firebase/Exercise/BOOKS/init-firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDzk0_derDzm_gZQN7F5fUfQMse2Hb9Y0I",
+    authDomain: "js-app-ex-rem-dbs-blogs-2024.firebaseapp.com",
+    projectId: "js-app-ex-rem-dbs-blogs-2024",
+    storageBucket: "js-app-ex-rem-dbs-blogs-2024.appspot.com",
+    messagingSenderId: "686447973401",
+    appId: "1:686447973401:web:8d7c6516f6277f94260688"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
