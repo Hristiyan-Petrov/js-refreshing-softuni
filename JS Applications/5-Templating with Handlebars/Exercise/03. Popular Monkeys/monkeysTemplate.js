cf1529c6e9a2028c333fd01ed06d12c3ee3e6dea
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-08 18:47:41 +0300

    Create toggle info button functionality

--- JS Applications/5-Templating with Handlebars/Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ JS Applications/5-Templating with Handlebars/Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -12,5 +12,19 @@
     let monkeysHtml = createMokeysTemplate({ monkeys });
     console.log(monkeys);
     divWrapper.innerHTML = monkeysHtml;
+
+    divWrapper.addEventListener('click', function (e) {
+        if (e.target.textContent !== 'Info') {
+            return;
+        }
+
+        console.log(infoElement.style.value);
+
+        if (infoElement.attributes.style.value === 'display: none' || infoElement.style.display === 'none') {
+            infoElement.style.display = 'block';
+        } else {
+            infoElement.style.display = 'none';
+        }
+    })
 })();
 
