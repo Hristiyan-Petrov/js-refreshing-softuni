c96a7e10ad552c2068783d325c581c240d99642d
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-08 18:41:43 +0300

    Create template business logic

--- JS Applications/5-Templating with Handlebars/Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ JS Applications/5-Templating with Handlebars/Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,4 +1,16 @@
-$(() => {
-    // TODO
+(() => {
     console.log('yeeeey');
-});
+
+    const divWrapper = document.querySelector('.monkeys');
+
+    const monkeyPartialSource = document.getElementById('monkey-partial-template').innerHTML;
+    Handlebars.registerPartial('monkeyPartial', monkeyPartialSource);
+
+    const allMonkeysParialSource = document.querySelector('#monkeys-template').innerHTML;
+    const createMokeysTemplate = Handlebars.compile(allMonkeysParialSource);
+
+    let monkeysHtml = createMokeysTemplate({ monkeys });
+    console.log(monkeys);
+    divWrapper.innerHTML = monkeysHtml;
+})();
+
