de2881635dc2d6aa0f918db6aafb729632da2f40
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-08 14:00:34 +0300

    Get out the business logic from html file - Separation of concerns

--- JS Applications/5-Templating with Handlebars/Exercise/01. List Towns/app.js
+++ JS Applications/5-Templating with Handlebars/Exercise/01. List Towns/app.js
@@ -1,5 +1,22 @@
-let towns = ['Sofia', 'Varna'];
+function loadTowns(e) {
+    e.preventDefault();
 
-let transformedTowns = towns.map(town => ({ name: town }));
+    // Create array of objects, each with property 'name' = {the name}
+    let inputEl = document.getElementById('towns');
+    let towns = inputEl.value
+        .split(', ')
+        .map(x => ({ name: x }));
 
-console.log(transformedTowns);
+    // Register partial template
+    let townView = document.getElementById('town-parial').innerHTML;
+    Handlebars.registerPartial('townPartial', townView);
+
+    // Set up the parent template
+    let townsView = document.getElementById('towns-view').innerHTML;
+    let createTownsHtml = Handlebars.compile(townsView); // Create function createTownsHtml()
+
+    // Create and append the html 
+    let townsFullHtml = createTownsHtml({ towns }); // Returns string
+    let rootElement = document.getElementById('root');
+    rootElement.innerHTML = townsFullHtml;
+}
