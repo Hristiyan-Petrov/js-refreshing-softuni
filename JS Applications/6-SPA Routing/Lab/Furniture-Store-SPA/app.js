2cc7bf8e88fe20078df12627515284777a596dab
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-10 13:18:20 +0300

    Create new furniture via form, add to db and redirect to all furniture

--- JS Applications/6-SPA Routing/Lab/Furniture-Store-SPA/app.js
+++ JS Applications/6-SPA Routing/Lab/Furniture-Store-SPA/app.js
@@ -1,24 +1,66 @@
-document.querySelector('#container').addEventListener('submit', function (e) {
+document.querySelector('#container').addEventListener('submit', handleForm);
+
+function handleForm(e) {
     let form = e.target;
     if (!form.nodeName === 'FORM') return;
-
     e.preventDefault();
-    const inputs = Array.from(form.elements);
 
-    inputs.forEach((input, i) => {
-        console.log(i);
-        // Skip if the input does not have the 'required' attribute 
-        if (!input.hasAttribute('required')) return;
+    // Define a flag to check if all inputs are valid
+    let allInputsValid = true;
 
-        if (!input.checkValidity()) {
-            input.classList.add('is-invalid');
-        } else {
-            input.classList.remove('is-invalid');
-            input.classList.add('is-valid');
-        }
-    });
-});
+    let inputs = Array.from(form.elements);
+    console.log(inputs[0]);
 
-// document.querySelector('form input.btn').addEventListener('click', function(e) {
-//     console.log(e.target);
-// })
+    validate();
+
+    if (!allInputsValid) return;
+
+    addFurnitureToDataBase();
+
+    function validate() {
+
+        inputs.forEach((input, i) => {
+            console.log(i);
+            // Skip if the input does not have the 'required' attribute 
+            if (!input.hasAttribute('required')) return;
+
+            if (!input.checkValidity()) {
+                input.classList.add('is-invalid');
+                allInputsValid = false;
+            } else {
+                input.classList.remove('is-invalid');
+                input.classList.add('is-valid');
+            }
+        });
+    }
+
+    function addFurnitureToDataBase() {
+        let url = 'https://js-apps-routing-lab-furniture-default-rtdb.firebaseio.com/furniture.json';
+
+        let bodyData = JSON.stringify({
+            'make': inputs[0].value,
+            'model': inputs[1].value,
+            'year': inputs[2].value,
+            'description': inputs[3].value,
+            'price': inputs[4].value,
+            'image': inputs[5].value,
+            'material': inputs[6].value,
+        });
+
+        fetch(url, {
+            method: 'POST',
+            body: bodyData
+        })
+            .then(res => res.json())
+            .then(furniture => {
+                console.log('successfully added to DB');
+                
+                history.pushState({}, '', '/furniture/all');
+                // location.replace('/furniture/all');
+                router();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
