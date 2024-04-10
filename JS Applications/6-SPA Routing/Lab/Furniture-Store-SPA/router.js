a79fa898e8912f1312a1e3ab80d03f0b35e4ecdc
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-10 15:33:08 +0300

    Code refactoring and optimizing functions

--- JS Applications/6-SPA Routing/Lab/Furniture-Store-SPA/router.js
+++ JS Applications/6-SPA Routing/Lab/Furniture-Store-SPA/router.js
@@ -13,8 +13,8 @@ async function router() {
     const app = document.getElementById('container');
 
     const routes = {
-        '/furniture/all': await getTemplateAll('allFurniture', dbUrl),
-        '/furniture/create': await getTemplateCreate('createFurniture'),
+        '/furniture/all': await getTemplate('allFurniture', dbUrl),
+        '/furniture/create': await getTemplate('createFurniture'),
     }
 
     console.log(route);
@@ -23,67 +23,31 @@ async function router() {
 
     if (route.startsWith('/furniture/details')) {
         let furnitureId = route.split('/').pop(); // get the last part of the path as id
-        let html = await getTemplateDetail('furnitureItemDetails', dbUrl, furnitureId);
-        containerHtml = html;
+        containerHtml = await getTemplate('furnitureItemDetails', dbUrl, furnitureId);
     } else {
         containerHtml = routes[route];
     }
     
-    app.innerHTML = routes[route] || '<h1>Page not found</h1>';
+    app.innerHTML = containerHtml || '<h1>Page not found</h1>';
 }
 
-function getTemplateCreate(templateLocation) {
-    return fetch(`${templateLocation}.hbs`)
-        .then(res => res.text())
-        .catch(err => {
-            console.log(err.message);
-        });
-}
 
-function getTemplateDetail(templateLocation, dbUrl, id) {
-    // Handle furniture item details route
-
+function getTemplate(templateLocation, dbUrl, id = "") {
     return Promise.all([
         fetch(`${templateLocation}.hbs`),
-        fetch(dbUrl + id + '.json')
+        fetch(dbUrl + id + ".json")
     ])
-        .then(([templateRes, furnitureRes]) => {
-            return Promise.all([templateRes.text(), furnitureRes.json()]);
-        })
-        .then(([template, furnitureItemData]) => {
-            console.log(template);
-            console.log(furnitureItemData);
+        .then(([templateRes, dataRes]) => Promise.all([templateRes.text(), dataRes.json()]))
+        .then(([template, data]) => {
+            let templateData = data;
 
-            let createHtml = Handlebars.compile(template);
-            // Return the html to render 
-            return createHtml(furnitureItemData);
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
-}
+            if (templateLocation === 'allFurniture') {
+                templateData = { 
+                    furniture: Object.keys(data).map(key => ({ id: key, ...data[key] }))
+                };
+            }
 
-function getTemplateAll(templateLocation, dbUrl) {
-    // Handle get all route
-
-    return Promise.all([
-        fetch(`${templateLocation}.hbs`),
-        fetch(dbUrl + '.json')
-    ])
-        .then(([templateRes, furnitureRes]) => {
-            return Promise.all([templateRes.text(), furnitureRes.json()]);
-        })
-        .then(([template, furnitureData]) => {
-            // Iterating over furnitureData object keys to generate a new array with id properties to be used in the template 
-            let dataWithId = Object.keys(furnitureData).map(key => ({
-                id: key,
-                ...furnitureData[key]
-            }));
-
-            // Pass the new array to Handlebars
-            let createHtml = Handlebars.compile(template);
-            // Return the html to render 
-            return createHtml({ furniture: dataWithId });
+            return Handlebars.compile(template)(templateData);
         })
         .catch(err => {
             console.log(err.message);
