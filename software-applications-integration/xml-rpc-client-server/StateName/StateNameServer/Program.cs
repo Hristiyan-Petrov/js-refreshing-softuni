bb4c04943cc46c1f6ec3fd965090d721b767b51d
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 17:42:08 +0300

    Add references to the StateNameServer project

--- software-applications-integration/xml-rpc-client-server/StateName/StateNameServer/Program.cs
+++ software-applications-integration/xml-rpc-client-server/StateName/StateNameServer/Program.cs
@@ -3,6 +3,3 @@ using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Http;
 using CookComputing.XmlRpc;
 using StateNameInterface;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
