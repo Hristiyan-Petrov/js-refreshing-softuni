367704c8f117ac2b2fa0f7ad4b1626d6e5785cd4
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 10:19:15 +0300

    Add StateStructRequest

--- Software apps integration/StateNameInterface/Class1.cs
+++ Software apps integration/StateNameInterface/Class1.cs
@@ -5,5 +5,13 @@ namespace StateNameInterface
     public interface IStateName
     {
 
+
     }
+
+    public struct StateStructRequest
+        {
+            public int state1;
+            public int state2;
+            public int state3;
+        }
 }
