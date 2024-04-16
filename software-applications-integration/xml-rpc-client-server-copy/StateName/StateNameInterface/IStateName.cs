a909f5048133c7804fc8359a0e3aaa24d7e431cc
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 19:49:39 +0300

    Get version from last commit because of file crash and screwing up the whole application

---
+++ software-applications-integration/xml-rpc-client-server-copy/StateName/StateNameInterface/IStateName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookComputing.XmlRpc;
+
+namespace StateNameInterface
+{
+    public struct StateStructRequest
+    {
+        public int state1;
+        public int state2;
+        public int state3;
+    }
+
+    public interface IStateName
+    {
+        [XmlRpcMethod("getStateName")]
+        string GetStateName(int stateNumber);
+
+        [XmlRpcMethod("getStateNames")]
+        string GetStateNames(StateStructRequest request);
+    }
+}
---
+++ software-applications-integration/xml-rpc-client-server-copy/StateName/StateNameServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using CookComputing.XmlRpc;
+using StateNameInterface;
+
+public class StateNameServer : MarshalByRefObject, IStateName
+{
+    string[] m_stateNames
+ = { "Alabama", "Alaska", "Arizona", "Arkansas",
+ "California", "Colorado", "Connecticut", "Delaware", "Florida",
+ "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+ "Kansas", "Kentucky", "Lousiana", "Maine", "Maryland",
+ "Massachusetts", "Michigan", "Minnesota", "Mississipi", "Missouri",
+"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+ "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+ "Oklahoma", "Oregon", "Pennsylviania", "Rhose Island",
+ "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+ "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin",
+ "Wyoming" };
+
+
+    public string GetStateName(int stateNumber)
+    {
+        if (stateNumber < 1 || stateNumber > m_stateNames.Length)
+            throw new XmlRpcFaultException(1, "Invalid state number");
+        return m_stateNames[stateNumber - 1];
+    }
+
+    public string GetStateNames(StateStructRequest request)
+    {
+        if (request.state1 < 1 || request.state1 > m_stateNames.Length)
+            throw new XmlRpcFaultException(1, "State number 1 invalid");
+        if (request.state2 < 1 || request.state2 > m_stateNames.Length)
+            throw new XmlRpcFaultException(1, "State number 1 invalid");
+        if (request.state3 < 1 || request.state3 > m_stateNames.Length)
+            throw new XmlRpcFaultException(1, "State number 1 invalid");
+        string ret = m_stateNames[request.state1 - 1] + " "
+        + m_stateNames[request.state2 - 1] + " "
+        + m_stateNames[request.state3 - 1];
+        return ret;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Create and initialize channel properties
+        IDictionary props = new Hashtable();
+        props["name"] = "MyHttpChannel";
+        props["port"] = 5678;
+
+        // Create a new HTTP channel for remote calls
+        HttpChannel channel = new HttpChannel(props, null, new XmlRpcServerFormatterSinkProvider());
+
+        // Register the HTTP channel
+        ChannelServices.RegisterChannel(channel, false);
+
+        // Register the server object type so clients can get a proxy to it
+        RemotingConfiguration.RegisterWellKnownServiceType(typeof(StateNameServer), "statename.rem", WellKnownObjectMode.Singleton);
+
+        // Wait for the user to hit <Enter>
+        Console.WriteLine("Press <ENTER> to shutdown");
+        Console.ReadLine();
+    }
+}
