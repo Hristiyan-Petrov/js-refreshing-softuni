0263cb91e622d564a54b4b0e7d270ac40deaf3a8
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 11:45:57 +0300

    Declarate channel for remote calling

--- Software apps integration/StateNameClient/Program.cs
+++ Software apps integration/StateNameClient/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using CookComputing.XmlRpc;
+using StateNameInterface;
+
+namespace StateNameClient
+{
+    class Program
+    {
+        bool bUseSoap = false;
+        if (args.Length > 0 && args[0] == 'SOAP')
+            bUseSoap = true;
+        HttpChannel chnl;
+        if (bUseSoap)
+            chnl = new HttpChannel();
+        else
+            chnl = new HttpChannel(null, new XmlRpcServerFormatterSinkProvider(), null);
+    }
+
+}
