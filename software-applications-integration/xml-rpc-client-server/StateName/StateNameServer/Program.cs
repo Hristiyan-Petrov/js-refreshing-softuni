927625dac3247dc3dd513a377dd7d6b62e5ea7b5
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 17:33:18 +0300

    Fix solution project folders (fix reference applying) and set .NET versoin to 4.8 for compatibality

---
+++ software-applications-integration/xml-rpc-client-server/StateName/StateNameServer/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using CookComputing.XmlRpc;
+using StateNameInterface;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
