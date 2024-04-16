436401ebb3d2553f5a96c8883b64516368083b8c
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-16 11:13:06 +0300

    Add package xmlrpcnet to StateNameServer directory

--- Software apps integration/StateNameInterface/Class1.cs
+++ Software apps integration/StateNameInterface/Class1.cs
@@ -4,14 +4,18 @@ namespace StateNameInterface
 {
     public interface IStateName
     {
-
+        [XmlRpcMethod("getStateName")]
+        string GetStateName(int stateNumber);
+        
+        [XmlRpcMethod("getStateNames")]
+        string GetStateNames(StateStructRequest request);
 
     }
 
     public struct StateStructRequest
-        {
-            public int state1;
-            public int state2;
-            public int state3;
-        }
+    {
+        public int state1;
+        public int state2;
+        public int state3;
+    }
 }
