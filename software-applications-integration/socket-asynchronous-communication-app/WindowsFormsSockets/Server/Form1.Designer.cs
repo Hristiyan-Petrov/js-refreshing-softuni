0c9b0006972e0dfa28cb3a98ccbb375ec04e7611
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-04-17 11:26:08 +0300

    Create form

--- software-applications-integration/socket-asynchronous-communication-app/WindowsFormsSockets/Server/Form1.Designer.cs
+++ software-applications-integration/socket-asynchronous-communication-app/WindowsFormsSockets/Server/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.richTextBoxSendMsg = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxReceivedMsg = new System.Windows.Forms.RichTextBox();
+            this.buttonStartListen = new System.Windows.Forms.Button();
+            this.buttonStopListen = new System.Windows.Forms.Button();
+            this.buttonSendMsg = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ServerIP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Broadcast Message To CLients";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Message Received From Clients";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Status Message:";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(80, 22);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 22);
+            this.textBoxIP.TabIndex = 5;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(80, 54);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPort.TabIndex = 6;
+            this.textBoxPort.Text = "8000";
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Location = new System.Drawing.Point(125, 403);
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMsg.TabIndex = 7;
+            this.textBoxMsg.Text = "None";
+            // 
+            // richTextBoxSendMsg
+            // 
+            this.richTextBoxSendMsg.Location = new System.Drawing.Point(12, 142);
+            this.richTextBoxSendMsg.Name = "richTextBoxSendMsg";
+            this.richTextBoxSendMsg.Size = new System.Drawing.Size(193, 173);
+            this.richTextBoxSendMsg.TabIndex = 8;
+            this.richTextBoxSendMsg.Text = "";
+            // 
+            // richTextBoxReceivedMsg
+            // 
+            this.richTextBoxReceivedMsg.Location = new System.Drawing.Point(283, 142);
+            this.richTextBoxReceivedMsg.Name = "richTextBoxReceivedMsg";
+            this.richTextBoxReceivedMsg.Size = new System.Drawing.Size(200, 203);
+            this.richTextBoxReceivedMsg.TabIndex = 9;
+            this.richTextBoxReceivedMsg.Text = "";
+            // 
+            // buttonStartListen
+            // 
+            this.buttonStartListen.Location = new System.Drawing.Point(366, 25);
+            this.buttonStartListen.Name = "buttonStartListen";
+            this.buttonStartListen.Size = new System.Drawing.Size(117, 23);
+            this.buttonStartListen.TabIndex = 10;
+            this.buttonStartListen.Text = "Start Listening";
+            this.buttonStartListen.UseVisualStyleBackColor = true;
+            // 
+            // buttonStopListen
+            // 
+            this.buttonStopListen.Location = new System.Drawing.Point(366, 57);
+            this.buttonStopListen.Name = "buttonStopListen";
+            this.buttonStopListen.Size = new System.Drawing.Size(117, 23);
+            this.buttonStopListen.TabIndex = 11;
+            this.buttonStopListen.Text = "Stop Listening";
+            this.buttonStopListen.UseVisualStyleBackColor = true;
+            // 
+            // buttonSendMsg
+            // 
+            this.buttonSendMsg.Location = new System.Drawing.Point(44, 322);
+            this.buttonSendMsg.Name = "buttonSendMsg";
+            this.buttonSendMsg.Size = new System.Drawing.Size(136, 23);
+            this.buttonSendMsg.TabIndex = 12;
+            this.buttonSendMsg.Text = "Send Message";
+            this.buttonSendMsg.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(408, 403);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 13;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSendMsg);
+            this.Controls.Add(this.buttonStopListen);
+            this.Controls.Add(this.buttonStartListen);
+            this.Controls.Add(this.richTextBoxReceivedMsg);
+            this.Controls.Add(this.richTextBoxSendMsg);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.RichTextBox richTextBoxSendMsg;
+        private System.Windows.Forms.RichTextBox richTextBoxReceivedMsg;
+        private System.Windows.Forms.Button buttonStartListen;
+        private System.Windows.Forms.Button buttonStopListen;
+        private System.Windows.Forms.Button buttonSendMsg;
+        private System.Windows.Forms.Button buttonClose;
     }
 }
 
