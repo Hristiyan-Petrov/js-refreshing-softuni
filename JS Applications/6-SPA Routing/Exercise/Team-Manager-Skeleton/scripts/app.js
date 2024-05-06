419c61174b9d0b28b89aaa8c2cbc7e7d28213219
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-06 17:01:00 +0300

    Configure Sammy initial logic and routes to #/home and #/login

--- JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
+++ JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
@@ -1,1 +1,27 @@
- // TODO
+const router = Sammy('#main', function () { //#main is the root element in which the content will be rendered
+
+    this.use('Handlebars', 'hbs'); // Present and define template angine and file extension to Sammy 
+
+    this.get('#/home', function () {
+        this.loadPartials({
+            'header': '../templates/common/header.hbs',
+            'footer': '../templates/common/footer.hbs'
+        }).then(function () {
+            this.partial('../templates/home/home.hbs')
+        });
+    });
+
+    this.get('#/login', function () {
+        this.loadPartials({
+            'header': '../templates/common/header.hbs',
+            'footer': '../templates/common/footer.hbs',
+            'login': '../templates/login/loginForm.hbs'
+        }).then(function () {
+            this.partial('../templates/login/loginForm.hbs');
+        });
+    });
+});
+
+(() => {
+    router.run('#/home'); // Load initial route on app start
+})();
