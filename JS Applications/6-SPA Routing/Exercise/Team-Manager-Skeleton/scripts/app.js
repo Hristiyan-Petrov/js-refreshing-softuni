5d41ea15b28e83bad53d64269b39eae2a60e25cc
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-06 17:13:29 +0300

    Add Sammy code explanation

--- JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
+++ JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
@@ -3,11 +3,11 @@ const router = Sammy('#main', function () { //#main is the root element in which
     this.use('Handlebars', 'hbs'); // Present and define template angine and file extension to Sammy 
 
     this.get('#/home', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
+        this.loadPartials({ // Register the partials used in the then template
+            'header': '../templates/common/header.hbs', // key name must be the same as the partial name
             'footer': '../templates/common/footer.hbs'
         }).then(function () {
-            this.partial('../templates/home/home.hbs')
+            this.partial('../templates/home/home.hbs') // Laod template
         });
     });
 
