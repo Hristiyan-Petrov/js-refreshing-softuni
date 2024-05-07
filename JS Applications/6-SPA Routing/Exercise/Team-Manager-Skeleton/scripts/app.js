2b17618985bdf441c9e83316c0ee13818e65f576
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-07 11:25:26 +0300

    Create partial registering function for code reuse

--- JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
+++ JS Applications/6-SPA Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
@@ -1,48 +1,55 @@
 const router = Sammy('#main', function () { //#main is the root element in which the content will be rendered
 
-    this.use('Handlebars', 'hbs'); // Present template angine and file extension for Sammy to compile 
+    // Present template angine and file extension for Sammy to compile 
+    this.use('Handlebars', 'hbs');
 
-    this.get('/home', function () {
-        this.loadPartials({ // Register the partials used in the then template
-            'header': '../templates/common/header.hbs', // key name must be the same as the partial name
-            'footer': '../templates/common/footer.hbs'
-        }).then(function () {
-            this.partial('../templates/home/home.hbs') // Laod template
-        });
+    // GET requests
+
+    this.get('/home', function (context) { // Context comes from Sammy out of the box
+        loadPartials(context)
+            .then(function () {
+                this.partial('../templates/home/home.hbs') // Load template
+            });
     });
 
-    this.get('/login', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
+    this.get('/login', function (context) {
+        loadPartials(context, {
             'loginForm': '../templates/login/loginForm.hbs'
-        }).then(function () {
-            this.partial('../templates/login/loginPage.hbs');
-        });
+        })
+            .then(function () {
+                this.partial('../templates/login/loginPage.hbs');
+            });
     });
 
-    this.get('/register', function() {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
+    this.get('/register', function (context) {
+        loadPartials(context, {
             'registerForm': '../templates/register/registerForm.hbs'
         })
-        .then(function() {
-            this.partial('../templates/register/registerPage.hbs')
-        });
+            .then(function () {
+                this.partial('../templates/register/registerPage.hbs')
+            });
     });
 
-    this.get('/about', function() {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
-        })
-        .then(function() {
-            this.partial('../templates/about/about.hbs')
-        });
+    this.get('/about', function (context) {
+        loadPartials(context)
+            .then(function () {
+                this.partial('../templates/about/about.hbs')
+            });
     });
 });
+
+// POST requests
 
 (() => {
     router.run('/home'); // Load initial route on app start
 })();
+
+function loadPartials(context, extraPartials = {}) {
+    const commonPartials = { // Register the partials used in the then template
+        'header': '../templates/common/header.hbs', // key name must be the same as the partial name
+        'footer': '../templates/common/footer.hbs'
+    };
+
+    let allPartials = Object.assign({}, commonPartials, extraPartials);
+    return context.loadPartials(allPartials);
+}
