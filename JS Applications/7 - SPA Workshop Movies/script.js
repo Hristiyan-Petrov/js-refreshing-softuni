ff25f9df4318f486f1c5c5cf97f37fc074bfb7c3
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-10 13:51:00 +0300

    Finish router function, create routes Mapping object and create login view to show

--- JS Applications/7 - SPA Workshop Movies/script.js
+++ JS Applications/7 - SPA Workshop Movies/script.js
@@ -1,13 +1,31 @@
+const routes = { // Mapping object, dictionary
+    'login': 'login-form-template' // Id of Handlebars template
+}
+
+
+// Router is responsible for rendering views on specific path / Adjuster, traffic cop, regulirovchik
+const router = path => {
+    const mainElement = document.getElementById('main');
+
+    let template = Handlebars.compile(document.getElementById(routes[path]).innerHTML); // Creates function which returns HTML
+
+    mainElement.innerHTML = template();
+};
+
 (function addEvenetListeners() {
-    document.querySelector('.navigation').addEventListener('click', e => {
-        e.preventDefault();
-
-        if (!e.target.classList.contains('nav-link')) { // Contains is key word for Nodelist API (In this case DOMTokenList); little different from .tagName attr
-            return;
-        }
-
-        let url = new URL(e.target.href); // Skip string operations
-        history.pushState({}, '', url.pathname); // Change route / url
-    });
+    document.querySelector('.navigation').addEventListener('click', naviagateHandler);
 
 })();
+
+function naviagateHandler(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('nav-link')) { // Contains is key word for Nodelist API (In this case DOMTokenList); little different from .tagName attr
+        return;
+    }
+
+    let url = new URL(e.target.href); // Skip string operations
+    history.pushState({}, '', url.pathname); // Change route / url
+
+    router(url.pathname.slice(1));
+}
