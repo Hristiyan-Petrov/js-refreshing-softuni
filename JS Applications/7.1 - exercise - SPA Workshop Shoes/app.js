072e64732e46996bafc959ef4ec60734f5f4d0b1
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-12 17:57:43 +0300

    Make header and footer work

--- JS Applications/7.1 - exercise - SPA Workshop Shoes/app.js
+++ JS Applications/7.1 - exercise - SPA Workshop Shoes/app.js
@@ -1,37 +1,71 @@
 const app = Sammy('#root', function () {
 
-    this.use('Handlebars', '.hbs'); // Say to Sammy to use Handlebars for template engine; and .hbs for file extension
+    this.use('Handlebars', 'hbs'); // Say to Sammy to use Handlebars for template engine; and .hbs for file extension
 
     // Home
 
     this.get('/home', function (context) { // Get Sammy functions is used to render views; Post and others not 
-        this.partial('./templates/homeGuest.hbs'); // Sammy out of the box function for loading partials
+
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/homeGuest.hbs'); // Sammy out of the box function for loading views, templates
+            });
+
     });
 
     // User routes
 
     this.get('/register', function (context) {
-        this.partial('./templates/register.hbs');
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/register.hbs');
+            });
+
     });
 
     this.get('/login', function (context) {
-        this.partial('./templates/login.hbs');
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/login.hbs');
+            });
+
     });
 
     // Offers routes
     this.get('/create-offer', function (context) {
-        this.partial('./templates/createOffer.hbs');
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/createOffer.hbs');
+            });
+
     });
 
     this.get('/edit-offer', function (context) {
-        this.partial('./templates/editOffer.hbs');
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/editOffer.hbs');
+            });
+
     });
 
     this.get('/details', function (context) {
-        this.partial('./templates/details.hbs');
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/details.hbs');
+            });
+
     });
 });
 
 (() => {
     app.run('/home'); // On initial app load, load this route
 })();
+
+
+// Function for loading partials
+function extendContext(context) {
+    return context.loadPartials({
+        'header': './partials/header.hbs',
+        'footer': './partials/footer.hbs'
+    });
+}
