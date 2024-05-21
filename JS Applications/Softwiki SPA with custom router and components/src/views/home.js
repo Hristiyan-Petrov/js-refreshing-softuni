ad9406426bca030ae6b7052ec5cb8e0b8e6ee05e
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-21 10:17:50 +0300

    Show 'No articles yet' on met condition.

--- JS Applications/Softwiki SPA with custom router and components/src/views/home.js
+++ JS Applications/Softwiki SPA with custom router and components/src/views/home.js
@@ -6,36 +6,40 @@ import articleItem from '../views/article-item.js';
 export default ({
     articles,
     navigationHandler,  // Comes from: ...props
-}) => {
-    return html`
+}) => html`
 <div class="content">
         <section class="js">
             <h2>JavaScript</h2>
             <div class="articles">
                 <!-- Render all articles; ? -> Optional chaining  -->
-                ${articles?.filter(x => x.category === 'js').map(x => articleItem({ ...x, navigationHandler }))}
+                ${articles?.some(x => x.category === 'js') 
+                    ? articles.filter(x => x.category === 'js').map(x => articleItem({ ...x, navigationHandler })) 
+                    : 'No articles yet.'}
             </div>
         </section>
         <section class="CSharp">
             <h2>C#</h2>
             <div class="articles">
-                ${articles?.filter(x => x.category === 'csharp').map(x => articleItem({ ...x, navigationHandler }))}
-                
+                ${articles?.some(x => x.category === 'csharp') 
+                    ? articles.filter(x => x.category === 'csharp').map(x => articleItem({ ...x, navigationHandler })) 
+                    : 'No articles yet.'}
             </div>
         </section>
         <section class="Java">
             <h2>Java</h2>
             <div class="articles">
-                ${articles?.filter(x => x.category === 'java').map(x => articleItem({ ...x, navigationHandler }))}
+                ${articles?.some(x => x.category === 'java') 
+                    ? articles.filter(x => x.category === 'java').map(x => articleItem({ ...x, navigationHandler })) 
+                    : 'No articles yet.'}
             </div>
         </section>
         <section class="Pyton">
             <h2>Pyton</h2>
             <div class="articles">
-                ${articles?.filter(x => x.category === 'python').map(x => articleItem({ ...x, navigationHandler }))}
-                
+                ${articles?.some(x => x.category === 'python') 
+                    ? articles.filter(x => x.category === 'python').map(x => articleItem({ ...x, navigationHandler })) 
+                    : 'No articles yet.'}
             </div>
         </section>
     </div>
 `;
-}
