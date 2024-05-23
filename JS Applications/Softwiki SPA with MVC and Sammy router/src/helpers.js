f412b979c2d08bedb6ea75bbb1e2a6163cda6294
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-23 15:29:31 +0300

    Create reusalbe function for attaching Handlebars templates

--- JS Applications/Softwiki SPA with MVC and Sammy router/src/helpers.js
+++ JS Applications/Softwiki SPA with MVC and Sammy router/src/helpers.js
@@ -1,3 +1,18 @@
+// Handlebars partials loader
+export async function addPartials(ctx) {
+    const partials = await Promise.all([
+        ctx.load('/templates/common/header.hbs'),
+        ctx.load('/templates/common/footer.hbs'),        
+    ]);
+
+    ctx.partials = {
+        header: partials[0],
+        footer: partials[1],
+    }
+}
+
+// User helpers
+
 export function setUserData(data) {
     sessionStorage.setItem('auth', JSON.stringify(data));
 }
