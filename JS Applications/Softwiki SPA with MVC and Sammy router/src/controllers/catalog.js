7d7e1b5f92ec0dc03e062c94ade929dd245bd63d
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-05-23 15:29:46 +0300

    Add home page static render

--- JS Applications/Softwiki SPA with MVC and Sammy router/src/controllers/catalog.js
+++ JS Applications/Softwiki SPA with MVC and Sammy router/src/controllers/catalog.js
@@ -1,5 +1,11 @@
 // Responsible for rendering articles
 
+import { addPartials } from "../helpers.js";
+
 export async function homePage() {
     console.log('homeee');
+
+    await addPartials(this);
+
+    this.partial('/templates/catalog/homePage.hbs');
 }
