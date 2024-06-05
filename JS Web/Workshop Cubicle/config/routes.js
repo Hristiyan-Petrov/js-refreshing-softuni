feecc371378cb4d6b1d7ccd2cad0e6ff70b8a946
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-05 10:35:30 +0300

    Add first action for home page. Render the page.

--- JS Web/Workshop Cubicle/config/routes.js
+++ JS Web/Workshop Cubicle/config/routes.js
@@ -1,3 +1,7 @@
 export default (app) => {
 
+    app.get('/', (req, res) => {
+        res.status(200);
+        res.render('index');    // Express-handlebars out of the box searches for file with that name in folder dir/views
+    });
 }
