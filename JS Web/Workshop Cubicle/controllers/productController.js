5367e4c5b264c5f39cb3db6519cef63e6e9de486
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-05 14:11:36 +0300

    Add page titles to layout

--- JS Web/Workshop Cubicle/controllers/productController.js
+++ JS Web/Workshop Cubicle/controllers/productController.js
@@ -3,12 +3,12 @@ const router = Router();
 
 // If the whole route is: '/products' load this. It is like '/products/'
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: 'Cubicle'});
 });
 
 // Route is '/products/create'
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create Cube'});
 });
 
 export default router;
