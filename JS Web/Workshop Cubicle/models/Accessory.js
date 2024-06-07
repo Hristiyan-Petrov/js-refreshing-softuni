4eb1ebe56c54d20871270aa99ab3fc78d8f2134b
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-07 13:35:46 +0300

    Create Accessory mongoose model

--- JS Web/Workshop Cubicle/models/Accessory.js
+++ JS Web/Workshop Cubicle/models/Accessory.js
@@ -1,5 +1,20 @@
-import DataBaseModel from "./DataBaseModel.js";
+import mongoose from 'mongoose';
 
-export default class Accessory extends DataBaseModel {
-    
-}
+const accessorySchema = new mongoose.Schema({
+    name: {
+        name: String,
+        required: true,
+    },
+    description: {
+        name: String,
+        required: true,
+        maxlength: [20, 'Description must be 50 characters or less']
+    },
+    imageUrl: {
+        name: String,
+        required: true,
+        match: [/^https?/, 'Please use a valid Image Url']
+    }
+});
+
+export default mongoose.model('Accessory', accessorySchema);
