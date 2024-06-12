2380be7100322629ec8366559a56777d4d3ca51f
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-12 15:28:39 +0300

    Implement insert method

--- Trees and Algorhitms/Create Binary Search Trees/BST-v2.js
+++ Trees and Algorhitms/Create Binary Search Trees/BST-v2.js
@@ -10,4 +10,26 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(data) {
+        if (this.root === null) {
+            this.root = new Node(data);
+        } else {
+            const node = this.root;
+
+            const searchTree = node => {
+                if (data < node.value && node.left !== null) {
+                    searchTree(node.left);
+                } else if (data < node.value) {
+                    node.left = new Node(data);
+                } else if (data > node.value && node.rigth !== null) {
+                    searchTree(node.rigth);
+                } else if (data > node.value) {
+                    node.rigth = new Node(data);
+                }
+            };
+            
+            return searchTree(node);
+        }
+    }
 }
