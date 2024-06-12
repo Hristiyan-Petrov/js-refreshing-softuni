0a3e27349b83709a33280aab1db496f96250f0ff
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-12 15:14:00 +0300

    Create base classes

---
+++ Trees and Algorhitms/Create Binary Search Trees/BST-v2.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = left;
+        this.rigth = rigth;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
