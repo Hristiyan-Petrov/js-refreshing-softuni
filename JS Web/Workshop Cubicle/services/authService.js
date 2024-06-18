4168fcfa929fa63094d8532c37d96470287b1d5c
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-18 14:10:38 +0300

    Remove unnecessary validations from authService. They were refactored with express-validator

--- JS Web/Workshop Cubicle/services/authService.js
+++ JS Web/Workshop Cubicle/services/authService.js
@@ -6,11 +6,6 @@ import config from '../config/config.js';
 export default {
     async register({ username, password, repeatPassword }) {
 
-        let isUsernameAvailable = !await User.findOne({ username });
-        if (!isUsernameAvailable) throw { message: 'Username is taken!' };
-
-        if (password !== repeatPassword) throw { message: 'Passwords must match!' }
-
         const salt = await bcrypt.genSalt(config.development.SALT_ROUNDS);
         const hash = await bcrypt.hash(password, salt);
 
@@ -19,15 +14,6 @@ export default {
     },
 
     async login({ username, password }) {
-
-        // get user from db
-        // let user = await User.findOne({ username });
-        // if (!user) throw { message: 'User not found!' };
-
-        // compare password hash
-        // let isMatch = await bcrypt.compare(password, user.password);
-        // if (!isMatch) throw { message: 'Password is wrong!' };
-
         // generate token
 
         // not good idea to put all info in the token
