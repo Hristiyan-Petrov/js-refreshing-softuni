d9f32fabf19f19932012b203b4a7ffd9963729a9
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-25 15:06:48 +0300

    Remove unnecessary files

--- JS Web/Softwiki SPA/client-vanilla-js/src/views/notFound.js
+++
@@ -1,8 +0,0 @@
-import { html } from 'lit-html';
-
-export default () => html`
-    <div class="container-404">
-        <h1>404</h1>
-        <p>Oops! The page you requested was not found. It seems like you've taken a wrong turn. Let's get you back home.</p>
-    </div>
-`;
