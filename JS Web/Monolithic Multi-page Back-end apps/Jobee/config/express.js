0b439d7e166d1bb42a0e57b898fb8a2b6299323e
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2024-06-30 22:31:11 +0300

    Rename styles folder from 'static' to 'public' applying best practice

--- JS Web/Monolithic Multi-page Back-end apps/Jobee/config/express.js
+++ JS Web/Monolithic Multi-page Back-end apps/Jobee/config/express.js
@@ -23,7 +23,7 @@ module.exports = app => {
   app.set('view engine', 'hbs');
   app.set('views', './views');
 
-  app.use('/static', express.static('static'));
+  app.use('/public', express.static('public'));
 
   app.use(express.urlencoded({ extended: true }));    // Parse form bodies from client
 
